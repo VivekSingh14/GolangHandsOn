6915ee3e8e94e2b5a0288beed9f27b3782d4d01f
vicky1411 <vivek.singh141@yahoo.com>
2021-05-29 20:31:58 +0530

    added some array operations

--- ArrayLearn/main.go
+++ ArrayLearn/main.go
@@ -14,4 +14,27 @@ func main() {
 	}
 	fmt.Print("length of an array: ", len(arr), "\n")
 	//fmt.Println(len(arr))
+
+	fmt.Println("traversing array using range inbuilt variable")
+	for i := range arr {
+		fmt.Println(arr[i])
+	}
+
+	fmt.Println("traversing array using for each ")
+	//took boolean array of size arr
+	var published [len(arr)]bool
+
+	published[0] = true
+	published[len(arr)-1] = true
+	for i, ok := range published {
+		if ok {
+			fmt.Println(arr[i])
+		}
+
+	}
+
+	numarr := [3]int{1, 2, 3}
+
+	fmt.Println(numarr)
+
 }
