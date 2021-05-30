d7e79551c40c754326afc3b13e9e24325b4b9070
vicky1411 <vivek.singh141@yahoo.com>
2021-05-30 22:51:43 +0530

    sorting the integers

--- slices/main.go
+++ slices/main.go
@@ -1,14 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	var arr []int
 
 	//append
-	arr = append(arr, 4)
-	arr = append(arr, 6)
 	arr = append(arr, 7)
+	arr = append(arr, 3)
+	arr = append(arr, 19)
+
+	fmt.Println(arr)
+
+	fmt.Println("after sorting")
+
+	sort.Ints(arr)
 
 	fmt.Println(arr)
 }
