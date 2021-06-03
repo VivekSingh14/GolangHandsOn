258a2074c44fef8e08575f1d1f105303e0778ddd
vicky1411 <vivek.singh141@yahoo.com>
2021-06-03 23:16:59 +0530

    cheking if key is present in the map after delete method performed

--- MapLearn/main.go
+++ MapLearn/main.go
@@ -12,22 +12,36 @@ func main() {
 
 	fmt.Println(myMap)
 
-	fmt.Println("****************")
+	fmt.Println("\n****************")
 	//assigning some key values to map
 	myMap[03] = 103
 
 	fmt.Println(myMap)
 
-	fmt.Println("************traversing a map***********")
+	fmt.Println("\n************traversing a map***********")
 	//traversing a map
 	for i := 0; i < len(myMap); i++ {
 		fmt.Println(myMap[i+1])
 	}
 
-	fmt.Println("***********traversing a map using for each or range loop********")
+	fmt.Println("\n***********traversing a map using for each or range loop********")
 
 	for id, roll := range myMap {
 		fmt.Println(id, " : ", roll)
 	}
 
+	fmt.Println("\n*******check the existance of key in map*******")
+
+	roll, ok := myMap[01]
+
+	fmt.Println("key is present: ", ok)
+	fmt.Println("and Value for that key is: ", roll, " \n ")
+
+	//03 deleted here.
+	fmt.Println("*********03 deleted here and checking whether it is deleted or not !**********")
+	delete(myMap, 03)
+
+	_, ok1 := myMap[03]
+
+	fmt.Println("is", 04, "available in the map ? ", ok1)
 }
