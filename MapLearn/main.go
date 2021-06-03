4834d21fb426aaa48b1c85a874c08ac57f216f34
vicky1411 <vivek.singh141@yahoo.com>
2021-06-03 22:57:43 +0530

    traversing the map using iteration

--- MapLearn/main.go
+++ MapLearn/main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 
+	//declaration and initialization
 	myMap := map[int]int{
 		01: 101,
 		02: 102,
@@ -12,9 +13,15 @@ func main() {
 	fmt.Println(myMap)
 
 	fmt.Println("****************")
-
+	//assigning some key values to map
 	myMap[03] = 103
 
 	fmt.Println(myMap)
+
+	fmt.Println("************traversing a map***********")
+	//traversing a map
+	for i := 0; i < len(myMap); i++ {
+		fmt.Println(myMap[i+1])
+	}
 
 }
