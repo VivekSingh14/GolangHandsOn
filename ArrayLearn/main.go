50accc459a65c796cf595d16f939b1181d54f00b
VivekSingh14 <vivek.singh141@yahoo.com>
2022-04-19 23:14:48 +0530

    Testing commit

--- ArrayLearn/main.go
+++ ArrayLearn/main.go
@@ -15,7 +15,7 @@ func main() {
 	fmt.Print("length of an array: ", len(arr), "\n")
 	//fmt.Println(len(arr))
 
-	fmt.Println("traversing array using range inbuilt variable")
+	fmt.Println("traversing the array using range inbuilt variable")
 	for i := range arr {
 		fmt.Println(arr[i])
 	}
