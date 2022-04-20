f1562d40cd32d1c330bfc7789ed9671fda6eb235
Vivek Singh <vivek.singh141@yahoo.com>
2022-04-20 08:22:45 +0530

    Users/vivek/dev (#4)
    * commit into dev branch
    
    * Print duplicate chars from string

---
+++ ProblemSolving/DuplicateCharsFromString.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	str := "abcbdeafe"
+	arr := []rune(str)
+
+	map1 := make(map[rune]int)
+
+	for i := range arr{
+		if _, ok := map1[arr[i]]; ok {
+			temp := map1[arr[i]]
+			temp +=1
+			map1[arr[i]] = temp
+		}else{
+			map1[arr[i]] = 1
+		}
+	}
+
+	for r, v := range map1 {
+		if v == 2{
+			fmt.Printf("%c \t %d \n", r, v)
+		}
+	}
+
+}
